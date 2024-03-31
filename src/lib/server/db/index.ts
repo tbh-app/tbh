429ae68efffd93bc76c0eddbc04e2169673edcd7
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2024-03-31 14:30:42 +0200

    fix: take 2

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,13 +1,9 @@
-import { DATABASE_URL } from '$env/static/private'
-import { Pool, neonConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { PrismaClient } from '@prisma/client'
-import ws from 'isomorphic-ws'
+import { DATABASE_URL } from "$env/static/private"
+import { Pool } from "@neondatabase/serverless"
+import { PrismaNeon } from "@prisma/adapter-neon"
+import { PrismaClient } from "@prisma/client"
 
-neonConfig.webSocketConstructor = ws
-const connectionString = `${DATABASE_URL}`
-
-const pool = new Pool({ connectionString })
-const adapter = new PrismaNeon(pool)
-export const prisma = new PrismaClient({ adapter })
+const neon = new Pool({ connectionString: DATABASE_URL })
+const adapter = new PrismaNeon(neon)
+const prisma = new PrismaClient({ adapter })
 export default prisma
